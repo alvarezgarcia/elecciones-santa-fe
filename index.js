d0c1c8ddf4f68004a438717c77f2e5a5851e9e99
Sebastián Alvarez <seba.alvarez16@gmail.com>
2019-06-16 18:42:49 -0300

    Nuevas cabeceras http para traer json

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ const babar = require('babar');
 
 const URL = 'https://elecciones.santafe.gob.ar/provincia/gobernador';
 
-const getElection = () => superagent.get(URL).set('X-Requested-With', 'XMLHttpRequest');
+const getElection = () => superagent
+  .get(URL)
+  .set('X-Requested-With', 'XMLHttpRequest')
+  .set('User-Agent', `Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36`)
+  .set('Accept', 'application/json, text/javascript, */*; q=0.01')
+
 const getPercentage = (votes, total) => (votes * 100) / total;
 const clear = 
 
@@ -17,34 +22,30 @@ setInterval( async  () => {
       cabecera 
     } = values;
 
+    console.log(cabecera);
+
+    const [ fpcysRaw, juntosRaw, cambiemosRaw ] = detalle;
+
     const fpcys = {
-      cantidadVotos: parseInt(detalle[0].cantidadOrden),
-      nombre: detalle[1].nombreCandidato
-    };
+      cantidadVotos: parseInt(fpcysRaw.cantidad),
+      nombre: fpcysRaw.candidato
+    }
 
     const juntos = {
-      cantidadVotos: parseInt(detalle[4].cantidadOrden),
-      nombre: detalle[4].nombre,
-      sumar: {
-        cantidadVotos: parseInt(detalle[5].cantidadOrden),
-        nombre: detalle[5].nombreCandidato
-      },
-      epsf: {
-        cantidadVotos: parseInt(detalle[6].cantidadOrden),
-        nombre: detalle[6].nombreCandidato
-      }
-    };
+      cantidadVotos: parseInt(juntosRaw.cantidad),
+      nombre: juntosRaw.candidato
+    }
 
     const cambiemos = {
-      cantidadVotos: parseInt(detalle[9].cantidadOrden),
-      nombre: detalle[10].nombreCandidato
-    };
+      cantidadVotos: parseInt(cambiemosRaw.cantidad),
+      nombre: cambiemosRaw.candidato
+    }
 
     const totalVotes = fpcys.cantidadVotos + juntos.cantidadVotos + cambiemos.cantidadVotos;
 
-    fpcys.percentage = getPercentage(fpcys.cantidadVotos, totalVotes);
-    juntos.percentage = getPercentage(juntos.cantidadVotos, totalVotes);
-    cambiemos.percentage = getPercentage(cambiemos.cantidadVotos, totalVotes);
+    fpcys.percentage = getPercentage(fpcys.cantidadVotos, totalVotes) || 0;
+    juntos.percentage = getPercentage(juntos.cantidadVotos, totalVotes) || 0;
+    cambiemos.percentage = getPercentage(cambiemos.cantidadVotos, totalVotes) || 0;
 
     process.stdout.write('\033c');
     
@@ -62,9 +63,12 @@ setInterval( async  () => {
       minY: 1,
     }));
 
-    console.log(`${fpcys.nombre}, ${fpcys.percentage.toFixed(2)}%`);
-    console.log(`${juntos.nombre}, ${juntos.percentage.toFixed(2)}%`);
-    console.log(`${cambiemos.nombre}, ${cambiemos.percentage.toFixed(2)}%`);
+    console.log('\n');
+    console.log(`0 - ${fpcys.nombre}, ${fpcys.percentage.toFixed(2)}%`);
+    console.log(`1 - ${juntos.nombre}, ${juntos.percentage.toFixed(2)}%`);
+    console.log(`2 - ${cambiemos.nombre}, ${cambiemos.percentage.toFixed(2)}%`);
+
+    console.log(`\Mesas: (${cabecera[0].mesasIngresadas}/${cabecera[0].totalMesas})`);
     console.log(`\nEscrutadas ${cabecera[0].porcentajeMesas}`);
     console.log(cabecera[0].fechaHoraInformacion);
 
